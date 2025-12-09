bbdb60da5eb22beb22b1f424e38237455caa900e
Boateng-Yaw-Edmund <edmundcharwey11@gmail.com>
2025-12-09 16:04:03 +0000

    Fix gitignore so Streamlit can access models

--- streamlit_app/models/model_test.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import numpy as np
-
-model = joblib.load("C:/Users/DELL 👑/Desktop/HNG 13/HNG_Stage8/streamlit_app/models/xgb_meta.joblib")
-scaler = joblib.load("C:/Users/DELL 👑/Desktop/HNG 13/HNG_Stage8/streamlit_app/models/meta_scaler.joblib")
-
-sample = np.array([[120, 25, 1, 12, 3, 10]])
-sample_s = scaler.transform(sample)
-print(model.predict_proba(sample_s))
